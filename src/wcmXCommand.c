f1ecd11c619f1f7eb7850dd2fb514307ca1dd1ce
Jan Steffens <jan.steffens@gmail.com>
2010-09-30 02:40:30 +0200

    Correct TPCButton property inversion
    The property had the inverse meaning (TPCButton on actually meant
    it was off). Correct this.
    
    Signed-off-by: Jan Steffens <jan.steffens@gmail.com>
    Reviewed-by: Ping Cheng <pinglinux@gmail.com>
    Tested-by: Jason Alavaliant <alavaliant@gmail.com>
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/wcmXCommand.c
+++ src/wcmXCommand.c
@@ -220,7 +220,7 @@ void InitWcmDeviceProperties(InputInfoPtr pInfo)
 	values[0] = common->wcmTouch;
 	prop_touch = InitWcmAtom(pInfo->dev, WACOM_PROP_TOUCH, 8, 1, values);
 
-	values[0] = !common->wcmTPCButton;
+	values[0] = common->wcmTPCButton;
 	prop_hover = InitWcmAtom(pInfo->dev, WACOM_PROP_HOVER, 8, 1, values);
 
 	values[0] = common->wcmGesture;
@@ -791,7 +791,7 @@ int wcmSetProperty(DeviceIntPtr dev, Atom property, XIPropertyValuePtr prop,
 			return BadValue;
 
 		if (!checkonly && common->wcmTPCButton != !values[0])
-			common->wcmTPCButton = !values[0];
+			common->wcmTPCButton = values[0];
 #ifdef DEBUG
 	} else if (property == prop_debuglevels)
 	{
