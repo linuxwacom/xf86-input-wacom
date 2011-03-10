301297d6423a3556fd94006d739a4afca82b5812
Peter Hutterer <peter.hutterer@who-t.net>
2011-03-10 10:12:48 +1000

    Re-vamp source inclusion for test build suite.
    test/Makefile.am included src/Makefile.am for the driver sources. However,
    that also lead to the driver being built twice, once in src/ and once in
    test/. On make install, test/wacom_drv.so would overwrite the
    src/wacom_drv.so and thus install a driver with all static symbols compiled
    as non-static.
    
    This caused the VCOPY bug worked around in
    9d227cd0bd985e7dcb7923974361145ba1d78713.
    
    Avoid this by moving the sources into a shared included Makefile and build
    an independent libwacom-test.la in the test directory.
    
    Reported-by: Eduard Hasenleithner <ehasenle@gmail.com>
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Tested-by: Eduard Hasenleithner <ehasenle@gmail.com>
    Acked-by: Ping Cheng <pinglinux@gmail.com>

---
+++ src/common.mk
@@ -0,0 +1,15 @@
+# shared makefile between src/Makefile.am and test/Makefile.am
+
+DRIVER_SOURCES= \
+	$(top_srcdir)/src/xf86Wacom.c \
+	$(top_srcdir)/src/xf86Wacom.h \
+	$(top_srcdir)/src/wcmCommon.c \
+	$(top_srcdir)/src/wcmConfig.c \
+	$(top_srcdir)/src/wcmISDV4.c \
+	$(top_srcdir)/src/wcmFilter.c \
+	$(top_srcdir)/src/wcmFilter.h \
+	$(top_srcdir)/src/xf86WacomDefs.h \
+	$(top_srcdir)/src/wcmUSB.c \
+	$(top_srcdir)/src/wcmXCommand.c \
+	$(top_srcdir)/src/wcmValidateDevice.c \
+	$(top_srcdir)/src/wcmTouchFilter.c
