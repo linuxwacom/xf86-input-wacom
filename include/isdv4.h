4f0f0e3510ac9afc671ac0dc2cf23e5ab7d5fb70
Peter Hutterer <peter.hutterer@who-t.net>
2014-03-18 10:20:53 +1000

    isdv4: inline all isdv4 parsing functions
    Stops compiler warning if any of them aren't used.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Acked-by: Jason Gerecke <killertofu@gmail.com>

--- include/isdv4.h
+++ include/isdv4.h
@@ -103,8 +103,8 @@ typedef struct {
 	uint8_t tilt_y;
 } ISDV4CoordinateData;
 
-static int isdv4ParseQuery(const unsigned char *buffer, const size_t len,
-		    ISDV4QueryReply *reply)
+static inline int isdv4ParseQuery(const unsigned char *buffer, const size_t len,
+				  ISDV4QueryReply *reply)
 {
 	int header, control;
 
@@ -130,8 +130,8 @@ static int isdv4ParseQuery(const unsigned char *buffer, const size_t len,
 	return ISDV4_PKGLEN_TPCCTL;
 }
 
-static int isdv4ParseTouchQuery(const unsigned char *buffer, const size_t len,
-			 ISDV4TouchQueryReply *reply)
+static inline int isdv4ParseTouchQuery(const unsigned char *buffer, const size_t len,
+				       ISDV4TouchQueryReply *reply)
 {
 	int header, control;
 
@@ -157,8 +157,8 @@ static int isdv4ParseTouchQuery(const unsigned char *buffer, const size_t len,
 }
 
 /* pktlen defines what touch type we parse */
-static int isdv4ParseTouchData(const unsigned char *buffer, const size_t buff_len,
-		        const size_t pktlen, ISDV4TouchData *touchdata)
+static inline int isdv4ParseTouchData(const unsigned char *buffer, const size_t buff_len,
+				      const size_t pktlen, ISDV4TouchData *touchdata)
 {
 	int header, touch;
 
@@ -191,8 +191,8 @@ static int isdv4ParseTouchData(const unsigned char *buffer, const size_t buff_le
 	return pktlen;
 }
 
-static int isdv4ParseCoordinateData(const unsigned char *buffer, const size_t len,
-			     ISDV4CoordinateData *coord)
+static inline int isdv4ParseCoordinateData(const unsigned char *buffer, const size_t len,
+					   ISDV4CoordinateData *coord)
 {
 	int header, control;
 
