baf91c9402b222b3f532d1f89a79d1cd77d4d821
Peter Hutterer <peter.hutterer@who-t.net>
2021-11-24 10:17:19 +1000

    Unifdef HAVE_CONFIG_H
    There's no case where this is not defined
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/wcmValidateDevice.c
+++ src/wcmValidateDevice.c
@@ -16,9 +16,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#ifdef HAVE_CONFIG_H
 #include <config.h>
-#endif
 
 #include "xf86Wacom.h"
 #include "wcmFilter.h"
--- src/xf86Wacom.c
+++ src/xf86Wacom.c
@@ -35,9 +35,7 @@
  * Peter Hutterer <peter.hutterer@redhat.com>.
  */
 
-#ifdef HAVE_CONFIG_H
 #include <config.h>
-#endif
 
 #include <sys/stat.h>
 #include <fcntl.h>
