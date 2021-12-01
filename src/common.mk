d254f902ef0452b63d39935b670814cdabe31534
Peter Hutterer <peter.hutterer@who-t.net>
2021-12-01 12:11:56 +1000

    Move the X-driver-specific files into src/x11
    These two files are the only ones that interact with the X server, let's
    move them so we have a visible separation between the core driver and
    the X parts.
    
    Renaming wcmXCommand.c to xf86WacomProperties.c in the process, for
    slightly better namespacing.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/common.mk
+++ src/common.mk
@@ -2,7 +2,8 @@
 
 DRIVER_SOURCES= \
 	$(top_srcdir)/src/WacomInterface.h \
-	$(top_srcdir)/src/xf86Wacom.c \
+	$(top_srcdir)/src/x11/xf86Wacom.c \
+	$(top_srcdir)/src/x11/xf86WacomProperties.c \
 	$(top_srcdir)/src/xf86Wacom.h \
 	$(top_srcdir)/src/wcmCommon.c \
 	$(top_srcdir)/src/wcmConfig.c \
@@ -11,7 +12,6 @@ DRIVER_SOURCES= \
 	$(top_srcdir)/src/wcmFilter.h \
 	$(top_srcdir)/src/xf86WacomDefs.h \
 	$(top_srcdir)/src/wcmUSB.c \
-	$(top_srcdir)/src/wcmXCommand.c \
 	$(top_srcdir)/src/wcmValidateDevice.c \
 	$(top_srcdir)/src/wcmTouchFilter.c \
 	$(top_srcdir)/src/wcmTouchFilter.h
